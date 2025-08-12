bd7219af97ed9daeb3644d995d28a188b99aef83
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-12 12:19:11 +0200

    feat: add Hero section with headline, subheading, and description for Home page

--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="flex flex-col justify-center items-center text-white gap-4">
+      <p className="font-semibold text-3xl">Helping Others</p>
+      <h1 className="font-bold text-5xl uppercase">Live & Travel</h1>
+      <p>Special offers to suit your plan</p>
+    </section>
+  );
 };
 
 export default Hero;
