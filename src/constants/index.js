3ef1394d21c9dfd70235296d0760cd1984c66c5e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-12 12:00:31 +0200

    feat: add NAV_LINKS and DESKTOP_LINKS constants with icon image paths for navigation

---
+++ src/constants/index.js
@@ -0,0 +1,35 @@
+export const NAV_LINKS = [
+  {
+    name: "Find Flights",
+    href: "/find-flights",
+    icon: "/assets/logos/airplane.png",
+  },
+  {
+    name: "Find Stays",
+    href: "/find-stays",
+    icon: "/assets/logos/ion_bed.png",
+  },
+  {
+    name: "Login",
+    href: "/login",
+    icon: "/assets/logos/IoEnter.png",
+  },
+  {
+    name: "Sign Up",
+    href: "/sign-up",
+    icon: "/assets/logos/IoIdCard.png",
+  },
+];
+
+export const DESKTOP_LINKS = [
+  {
+    name: "Find Flights",
+    href: "/find-flights",
+    icon: "/assets/logos/airplane.png",
+  },
+  {
+    name: "Find Stays",
+    href: "/find-stays",
+    icon: "/assets/logos/ion_bed.png",
+  },
+];
