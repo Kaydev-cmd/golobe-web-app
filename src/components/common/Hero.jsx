7a79b9f781c613d383ba4aa6a409e22c8bb79f6d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-13 07:17:25 +0200

    style: remove top padding from element

--- src/components/common/Hero.jsx
+++ src/components/common/Hero.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col justify-center items-center text-white gap-4">
+    <section
+      className="flex flex-col justify-center items-center text-white gap-4"
+      style={{ paddingTop: "0" }}
+    >
       <p className="font-semibold text-3xl">Helping Others</p>
       <h1 className="font-bold text-5xl uppercase">Live & Travel</h1>
       <p>Special offers to suit your plan</p>
