a4d6d4c303272d91a2b0431283142481fe469724
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-14 10:55:17 +0200

    style: update ReviewsCard styling for horizontal layout with hover effect and shadow

--- src/components/common/TripsCard.jsx
+++ src/components/common/TripsCard.jsx
@@ -3,7 +3,7 @@ import { LuDot } from "react-icons/lu";
 
 const TripsCard = ({ img, title }) => {
   return (
-    <div className="card bg-white p-8 flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer">
+    <div className="card bg-white flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer">
       <img src={img} alt={title} className="w-full" />
       <div className="flex flex-col gap-1">
         <h1 className="text-slate-500 font-semibold">{title}</h1>
