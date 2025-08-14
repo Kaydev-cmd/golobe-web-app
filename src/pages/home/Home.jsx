7aac479ea053937a252a2c6b8937dc62ac8551b4
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-14 11:01:26 +0200

    feat: render Reviews section component on page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Trips from "../../components/sections/Trips";
+import Reviews from "../../components/sections/Reviews";
 
 const Home = () => {
   return (
     <>
       <Trips />
+      <Reviews />
     </>
   );
 };
