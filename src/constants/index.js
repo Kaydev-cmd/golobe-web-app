0289af7839b20df6b946cd7b13272c42e1a2efe3
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-14 11:00:49 +0200

    feat: add REVIEWS_CARD_DATA array with sample review details including title, desscription, usser info, and images

--- src/constants/index.js
+++ src/constants/index.js
@@ -77,12 +77,40 @@ export const FLIGHTS_AND_HOTELS_DATA = [
   {
     backgroundImage: "/assets/images/cardBackground_1.png",
     title: "Flights",
-    description: "Book domestic and international flights at competitive prices with flexible scheduling options."
+    description:
+      "Book domestic and international flights at competitive prices with flexible scheduling options.",
   },
   {
     backgroundImage: "/assets/images/cardBackground_2.png",
     title: "Hotels",
-    description: "Find and reserve hotels worldwide with options ranging from budget stays to luxury resorts."
+    description:
+      "Find and reserve hotels worldwide with options ranging from budget stays to luxury resorts.",
+  },
+];
 
+export const REVIEWS_CARD_DATA = [
+  {
+    title: "A real sense of community, nurtured",
+    description:
+      "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.",
+    username: "Olga",
+    location: "Weave Studios - Kai Tak",
+    image: "/assets/images/Review_1.png",
+  },
+  {
+    title: "The facilities are superb. Clean, slick, bright.",
+    description:
+      "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.",
+    username: "Thomas",
+    location: "Weave Studios - Olympic",
+    image: "/assets/images/Review_2.png",
+  },
+  {
+    title: "A real sense of community, nurtured",
+    description:
+      "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.",
+    username: "Eliot",
+    location: "Weave Studios - Kai Tak",
+    image: "/assets/images/Review_3.png",
   },
 ];
