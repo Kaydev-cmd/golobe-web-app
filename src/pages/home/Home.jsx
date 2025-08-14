8d7921777faab31fca319f7b3931eb41ca46a04e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-14 14:27:07 +0200

    feat: render <Subscribe /> component on Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Trips from "../../components/sections/Trips";
 import Reviews from "../../components/sections/Reviews";
+import Subscribe from "../../components/common/Subscribe";
 
 const Home = () => {
   return (
     <>
       <Trips />
       <Reviews />
+      <Subscribe />
     </>
   );
 };
