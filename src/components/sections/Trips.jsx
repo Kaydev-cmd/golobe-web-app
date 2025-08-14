a2a4c165cddf403461d9855012a3b26904673d14
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-14 10:59:42 +0200

    refactor: update Reviews section, adjust button visibility, add gap sspacing, and pass description prop to cards

--- src/components/sections/Trips.jsx
+++ src/components/sections/Trips.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FLIGHTS_AND_HOTELS_DATA, TRIPS_CARD_DATA } from "../../constants";
 import TripsCard from "../common/TripsCard";
 import FlightsAndHotelsCard from "../common/FlightsAndHotelsCard";
+import Button from "../common/Button";
 
 const Trips = () => {
   return (
@@ -16,27 +17,19 @@ const Trips = () => {
             Search Flights & Places Hire to our most popular destinations.
           </p>
         </div>
-        <button
-          className="hidden border border-green-600"
-          style={{ borderRadius: "6px" }}
-        >
-          See more places
-        </button>
+        <div className="hidden">
+          <Button title="See more places" variant="secondary" />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 gap-4">
         {TRIPS_CARD_DATA.map((trip) => (
           <TripsCard key={trip.title} img={trip.image} title={trip.title} />
         ))}
-        <button
-          className="border border-green-600"
-          style={{ borderRadius: "8px", margin: "20px 0" }}
-        >
-          See more places
-        </button>
+        <Button title="See more places" variant="secondary" />
       </div>
 
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4" style={{ marginTop: "20px" }}>
         {FLIGHTS_AND_HOTELS_DATA.map((card) => (
           <div
             key={card.title}
@@ -45,7 +38,10 @@ const Trips = () => {
               backgroundImage: `url(${card.backgroundImage})`,
             }}
           >
-            <FlightsAndHotelsCard title={card.title} description={card.description}/>
+            <FlightsAndHotelsCard
+              title={card.title}
+              description={card.description}
+            />
           </div>
         ))}
       </div>
