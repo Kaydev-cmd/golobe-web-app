d4432d4df8a69ab02b931fe06450ff0afca2e377
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-19 07:58:10 +0200

    feat: add routes for Flights, Stays, Login, Signup, and PlaceDetails pages

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../components/layout/Main";
 import Home from "../pages/home/Home";
+import Flights from "../pages/flights/Flights";
+import Stays from "../pages/stays/Stays";
+import Login from "../pages/login/Login";
+import SignUp from "../pages/signup/SignUp";
+import PlaceDetail from "../components/common/PlaceDetail";
 
 const Router = createBrowserRouter([
   {
@@ -10,6 +15,26 @@ const Router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/flights",
+        element: <Flights />,
+      },
+      {
+        path: "/stays",
+        element: <Stays />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/place/:id",
+        element: <PlaceDetail />,
       },
     ],
   },
