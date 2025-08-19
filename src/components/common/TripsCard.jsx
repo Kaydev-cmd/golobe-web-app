1ff93eda746e82ce86212f6a4d8610c640189519
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-19 07:52:54 +0200

    feat: add onClick handler to reigger actions on user interaction

--- src/components/common/TripsCard.jsx
+++ src/components/common/TripsCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { LuDot } from "react-icons/lu";
 
-const TripsCard = ({ img, title }) => {
+const TripsCard = ({ img, title, onClick }) => {
   return (
-    <div className="card bg-white flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer">
+    <div onClick={onClick} className="card bg-white flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer">
       <img src={img} alt={title} className="w-full" />
       <div className="flex flex-col gap-1">
         <h1 className="text-slate-500 font-semibold">{title}</h1>
