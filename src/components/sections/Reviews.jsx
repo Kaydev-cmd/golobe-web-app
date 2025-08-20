cc28c6bcb8f72d5177b5bf41c9101e630bd8fe48
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-20 14:57:03 +0200

    feat: show/hide review buttons based on screen size

--- src/components/sections/Reviews.jsx
+++ src/components/sections/Reviews.jsx
@@ -6,19 +6,21 @@ import ReviewsCard from "../common/ReviewsCard";
 const Reviews = () => {
   return (
     <section className="container" style={{ paddingTop: "0" }}>
-      <div className="flex flex-col justify-center gap-4">
-        <div
-          className="flex flex-col gap-2 text-center"
-          style={{ marginBottom: "16px" }}
-        >
-          <h1 className="font-bold text-2xl">Reviews</h1>
-          <p className="text-slate-500">What people say about Golobe</p>
-        </div>
-        <div className="hidden">
-          <Button title="See more places" variant="secondary" />
+      <div className="flex flex-col justify-center gap-4 ">
+        <div className="flex flex-col justify-center gap-4 md:flex-row md:items-center md:justify-between">
+          <div
+            className="flex flex-col gap-2 text-center md:text-start"
+            style={{ marginBottom: "16px" }}
+          >
+            <h1 className="font-bold text-2xl">Reviews</h1>
+            <p className="text-slate-500">What people say about Golobe</p>
+          </div>
+          <div className="hidden md:flex">
+            <Button title="See more" variant="secondary" />
+          </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {REVIEWS_CARD_DATA.map((review) => (
             <ReviewsCard
               key={review.username}
@@ -30,7 +32,11 @@ const Reviews = () => {
             />
           ))}
         </div>
-
+      </div>
+      <div
+        className="flex justify-center md:hidden"
+        style={{ marginTop: "20px" }}
+      >
         <Button title="See All" variant="secondary" />
       </div>
     </section>
