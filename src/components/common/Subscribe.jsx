3536b0b94a551bfe0652ffa69110a965cdc784d9
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-20 14:53:37 +0200

    style: set medium screen width and height to 40% for responsive layout

--- src/components/common/Subscribe.jsx
+++ src/components/common/Subscribe.jsx
@@ -25,36 +25,45 @@ const Subscribe = () => {
 
   return (
     <section className="container" style={{ paddingTop: "0" }}>
-      <div className="card flex flex-col gap-3 bg-[#CDEAE1] rounded-xl">
-        <h1 className="text-3xl font-semibold">Subscribe Newsletter</h1>
-        <h2 className="text-xl font-semibold">The Travel</h2>
-        <p className="text-slate-600">
-          Get inspired! Recieve travel discounts, tips, and behind the scenes
-          stories.
-        </p>
-        <form className="flex flex-col gap-3">
-          <input
-            type="email"
-            placeholder="Your email address"
-            name="email"
-            style={{ borderRadius: "8px" }}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button className="bg-green-800 text-white" onClick={handleSubscribe}>
-            Subscribe
-          </button>
-          {message && (
-            <p
-              className={`text-sm text-center ${
-                isSuccess ? "text-green-600" : "text-red-500"
-              }`}
-              style={{ marginTop: "12px" }}
+      <div className="card flex flex-col gap-3 items-center md:flex-row md:gap-6 lg:justify-around bg-[#CDEAE1] rounded-xl">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-3xl font-semibold">Subscribe Newsletter</h1>
+          <h2 className="text-xl font-semibold">The Travel</h2>
+          <p className="text-slate-600">
+            Get inspired! Recieve travel discounts, tips, and behind the scenes
+            stories.
+          </p>
+          <form className="flex flex-col gap-3">
+            <input
+              type="email"
+              placeholder="Your email address"
+              name="email"
+              style={{ borderRadius: "8px" }}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+              className="bg-green-800 text-white"
+              onClick={handleSubscribe}
             >
-              {message}
-            </p>
-          )}
-        </form>
-        <img src="/assets/images/mailbox.png" alt="Mailbox" />
+              Subscribe
+            </button>
+            {message && (
+              <p
+                className={`text-sm text-center ${
+                  isSuccess ? "text-green-600" : "text-red-500"
+                }`}
+                style={{ marginTop: "12px" }}
+              >
+                {message}
+              </p>
+            )}
+          </form>
+        </div>
+        <img
+          src="/assets/images/mailbox.png"
+          alt="Mailbox"
+          className="w-full md:w-[40%] md:h-[40%]"
+        />
       </div>
     </section>
   );
