39bf13425cdddf91531d72e35e912309a47fa06e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-20 14:54:41 +0200

    style: set element width to 1/3 for layout adjustment

--- src/components/common/TripsCard.jsx
+++ src/components/common/TripsCard.jsx
@@ -3,8 +3,11 @@ import { LuDot } from "react-icons/lu";
 
 const TripsCard = ({ img, title, onClick }) => {
   return (
-    <div onClick={onClick} className="card bg-white flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer">
-      <img src={img} alt={title} className="w-full" />
+    <div
+      onClick={onClick}
+      className="card bg-white flex items-center gap-3 rounded-xl shadow-md transition-transform hover:scale-105 cursor-pointer"
+    >
+      <img src={img} alt={title} className="w-1/3 rounded-lg" />
       <div className="flex flex-col gap-1">
         <h1 className="text-slate-500 font-semibold">{title}</h1>
         <div className="flex items-center">
