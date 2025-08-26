c5269d674939e7a008a6ad37394c3aeb19e1fe3a
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-26 15:06:12 +0200

    refactor: pass flight.id as key and id props for FlightListingCard component

--- src/pages/flight-listing/FlightListing.jsx
+++ src/pages/flight-listing/FlightListing.jsx
@@ -24,6 +24,8 @@ const FlightListing = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {FLIGHT_LISITING_DATA.map((flight) => (
           <FlightListingCard
+            key={flight.id}
+            id={flight.id}
             image={flight.image}
             alt={flight.alt}
             rating={flight.rating}
