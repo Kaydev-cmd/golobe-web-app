85853b2a6737b2e129b8d191c5b849f87a8247ce
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-26 10:34:50 +0200

    feat: add Hero component to the page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Trips from "../../components/sections/Trips";
 import Reviews from "../../components/sections/Reviews";
 import Subscribe from "../../components/common/Subscribe";
+import Hero from "../../components/sections/Hero";
 
 const Home = () => {
   return (
     <>
+      <Hero />
       <Trips />
       <Reviews />
       <Subscribe />
